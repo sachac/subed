a58b002fe9a37086e61456463ffe8650bdb2946f
Sacha Chua <sacha@sachachua.com>
2022-01-02 18:49:08 -0500

    Clean up undercover-init.el

--- tests/undercover-init.el
+++ tests/undercover-init.el
@@ -1,9 +1,4 @@
-(add-to-list 'load-path "./subed")
-(add-to-list 'load-path "../subed")
+(add-to-list 'load-path (expand-file-name "./subed"))
 (when (require 'undercover nil t)
-  (undercover "./subed/*.el" "../subed/*.el" (:report-format 'simplecov) (:send-report nil)))
-
-;; (load "subed/subed.el")
-;; (load "subed/subed-srt.el")
-;; (load "subed/subed.el")
+  (undercover "./subed/*.el" (:report-format 'simplecov) (:send-report nil)))
 
