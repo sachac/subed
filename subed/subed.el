9a660ed88600e9aff741051c28a9e638cd5df5f5
Sacha Chua <sacha@sachachua.com>
2022-02-01 23:56:44 -0500

    Fix subed-regenerate-ids for SRT
    * subed/subed-srt.el (subed-srt--regenerate-ids): Fix regenerate-ids
    implementation in subed.

--- subed/subed.el
+++ subed/subed.el
@@ -1,6 +1,6 @@
 ;;; subed.el --- A major mode for editing subtitles  -*- lexical-binding: t; -*-
 
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Keywords: convenience, files, hypermedia, multimedia
 ;; URL: https://github.com/rndusr/subed
 ;; Package-Requires: ((emacs "25.1"))
