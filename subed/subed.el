badbfb7d2088efb1f8032ca6eb6f67b83f1af76a
Sacha Chua <sacha@sachachua.com>
2022-07-22 10:09:12 -0400

    Increment the version number to 1.0.6

--- subed/subed.el
+++ subed/subed.el
@@ -1,6 +1,6 @@
 ;;; subed.el --- A major mode for editing subtitles  -*- lexical-binding: t; -*-
 
-;; Version: 1.0.5
+;; Version: 1.0.6
 ;; Maintainer: Sacha Chua <sacha@sachachua.com>
 ;; Author: Random User
 ;; Keywords: convenience, files, hypermedia, multimedia
