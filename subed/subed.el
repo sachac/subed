43fcde0c3aad7e897f1fdb9be46b320e2d2335bf
Sacha Chua <sacha@sachachua.com>
2023-03-07 18:40:13 -0500

    v1.1.0 Mention subed-enforce-time-boundaries values

--- subed/subed.el
+++ subed/subed.el
@@ -1,6 +1,6 @@
 ;;; subed.el --- A major mode for editing subtitles  -*- lexical-binding: t; -*-
 
-;; Version: 1.0.29
+;; Version: 1.1.0
 ;; Maintainer: Sacha Chua <sacha@sachachua.com>
 ;; Author: Random User
 ;; Keywords: convenience, files, hypermedia, multimedia
